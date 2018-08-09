2cb688568d859e790ef603a6355374d0ea521ec5
Tian Zhang <tizhang@CNtizhang.local>
2018-08-09 23:14:10 +0800

    add hystrix in zuul
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/main/java/com/thoughtworks/zhangtian/training/zuul/ZuulApplication.java
+++ src/main/java/com/thoughtworks/zhangtian/training/zuul/ZuulApplication.java
@@ -2,9 +2,13 @@ package com.thoughtworks.zhangtian.training.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableZuulProxy
 @SpringBootApplication
--- src/main/java/com/thoughtworks/zhangtian/training/zuul/security/WebSecurityConfig.java
+++ src/main/java/com/thoughtworks/zhangtian/training/zuul/security/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/users", "/login", "/validate").permitAll()
-                .antMatchers("/health").permitAll()
+                .antMatchers("/health", "/hystrix*/**", "/webjars/**", "/proxy*").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(todoAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint);
